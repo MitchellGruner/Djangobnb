202c4af04f8333bb81606562a1aa4b13b485f747
Mitchell Gruner <mit.gruner@gmail.com>
2024-03-18 12:36:34 -0500

    add dummy text to <main />

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      
+      Django Bnb
+
+      <h2 className="text-airbnb-dark">Django Header</h2>
     </main>
   );
 }
