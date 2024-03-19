0f39b453232c2b57a526d7d264b78022de6d6637
Mitchell Gruner <mit.gruner@gmail.com>
2024-03-19 11:30:50 -0500

    initial commit of /Contact/ContactButton.tsx

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,7 @@
+const ContactButton = () => {
+    return (
+        <div className="mt-6 py-4 px-6 cursor-pointer bg-airbnb text-white rounded-xl hover:bg-airbnb-dark transition ease-in-out duration-500">Contact</div>
+    )
+}
+
+export default ContactButton;
