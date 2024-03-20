731f5cdaf18591826a94b6ea1474c3f031e015c9
Mitchell Gruner <mit.gruner@gmail.com>
2024-03-20 10:32:14 -0500

    add form content

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -3,13 +3,28 @@
 import { useState } from "react";
 
 import Modal from "./Modal";
+import CustomButton from "../forms/CustomButton";
 import useLoginModal from "@/app/hooks/useLoginModal";
 
 const LoginModal = () => {
     const loginModal = useLoginModal();
 
     const content = (
-        <h2 className="mb-6 text-2xl">Welcome to Djangobnb, please log in</h2>
+        <>
+            <form className="space-y-4">
+                <input placeholder="Your email address" type="email" className="w-full h-[54px] border px-4 border-gray-300 rounded-xl" />
+                <input placeholder="Your password" type="password" className="w-full h-[54px] border px-4 border-gray-300 rounded-xl" />
+
+                <div className="p-5 bg-airbnb text-white rounded-xl opacity-80">
+                    The error message
+                </div>
+            
+                <CustomButton
+                    label="Submit"
+                    onClick={() => console.log("Clicked")}
+                />
+            </form>
+        </>
     )
 
     return (
